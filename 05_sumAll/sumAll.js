bab524a5d528e6395c5fce412a34a0165339458e
Pablo <berwigpablo@gmail.com>
2024-06-01 10:36:33 -0300

    Finish exercise 5
    Had to use different method to check if arguments
    were Numbers due to object comparison. Used
    isInteger function instead.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sumTotal = 0;
 
+    if(start < 0 || 
+        end < 0 || 
+        !Number.isInteger(start) || 
+        !Number.isInteger(end)){
+        return 'ERROR'
+    } else {
+
+        if (start > end){
+            let switchNum = end;
+            end = start;
+            start = switchNum;
+        }
+
+        for(i = start; i <= end; i++){
+            sumTotal += i;
+        }
+
+        return sumTotal;
+    }
 };
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll;    
+
+
