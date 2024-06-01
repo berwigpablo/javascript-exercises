b49086592f9855560576bec33bc6b80f60f1ed39
Pablo <berwigpablo@gmail.com>
2024-06-01 10:55:09 -0300

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degFahrenheit) {
+  let degCelsius = (degFahrenheit - 32) * (5 / 9) //Fahrenheit to Celsius equation
+
+  if (Math.floor(degCelsius) !== degCelsius){
+    degCelsius = degCelsius.toFixed(1) 
+  }
+  return Number(degCelsius)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degCelsius) {
+  let degFahrenheit = (degCelsius / (5 / 9)) + 32 //Celsius to Fahrenheit equation
+
+  if (Math.floor(degFahrenheit) !== degFahrenheit){
+    degFahrenheit = degFahrenheit.toFixed(1) 
+  }
+  return Number(degFahrenheit)
+
 };
 
 // Do not edit below this line
