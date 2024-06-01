615ca54ee5602ca26eeb7c626fd0a9f63968635b
Pablo <berwigpablo@gmail.com>
2024-06-01 09:55:27 -0300

    Finish up to exercise 4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num === 0) return '';
+    if (num < 0) return 'ERROR';
 
+    let originalString = string;
+
+    for (i = 1; i < num; i++){
+        string += originalString;
+    }
+
+    return string;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeValue) {
+
+    filterArray = array.filter(checkValue)
+    
+    function checkValue(value){
+        for (i = 0; i < removeValue.length; i++){
+            if(value === removeValue[i]){
+                return false
+            }
+        }
+
+        return true;
+    }
+
+    return filterArray;
 };
 
 // Do not edit below this line
