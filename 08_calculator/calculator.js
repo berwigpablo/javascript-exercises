eb6571eff731987bee094581f079c1b42cae982c
Pablo <berwigpablo@gmail.com>
2024-06-10 22:00:21 -0300

    Finish calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(...numbers) {
+  return numbers.reduce((total, number) => {
+    if(numbers.length === 0){
+      return 0
+    } else{
+      return +total + +number
+    }
+  }, 0)
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  if(numbers.length === 0) return 0
+	return numbers.reduce((total, number) => total + number);
+}
+const multiply = function(numbers) {
+  if(numbers.length === 0) return 0
+  return numbers.reduce((total, number) => total * number)
 };
 
-const multiply = function() {
+const power = function(a, b) {
+	return a ** b
+};  
 
-};
+const factorial = function(number) {
+	if(number === 0 || number === 1) return 1 
+  let result = 0;
 
-const power = function() {
-	
-};
+  for(i = number; i > 1; i--){
+    if(i === number){
+      result += i * (i - 1);
+    } else{
+      result *= (i - 1);
+    }
+  }
 
-const factorial = function() {
-	
+  return result
 };
 
 // Do not edit below this line
