7889c298d1fe40ac965c1018f6ad345482d881cd
Pablo <berwigpablo@gmail.com>
2024-06-11 20:49:29 -0300

    Finish fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0) return 'OOPS'
 
+    if(num == 0){
+        return 0
+    } else if(num == 1){
+        return 1
+    }else{
+        firstNum = 0;
+        secondNum = 1;
+
+        for(i = 1; i < num; i++){
+            nextNum = firstNum + secondNum;
+            firstNum = secondNum;
+            secondNum = nextNum;
+        }
+
+        return nextNum
+    }
 };
 
 // Do not edit below this line
