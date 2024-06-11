a7a7077ecf21168426281d93f63959a84b852994
Pablo <berwigpablo@gmail.com>
2024-06-11 20:33:54 -0300

    Finish palindromes exercise without RegEx

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    removeChar = string.toLowerCase().split('').filter(char => {
+      if(char === ' ' || 
+        char === '!' || 
+        char === ',' || 
+        char === '.'){
+
+        return false
+
+      }else return true
+    }).join('');
+
+    console.log(removeChar);
+
+    if (removeChar.split('').reverse().join('') === removeChar){
+        return true
+    } else return false
 
 };
 
