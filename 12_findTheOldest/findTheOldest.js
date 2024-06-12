7a126d127f90913c5186c40a8e50718969fccc70
Pablo <berwigpablo@gmail.com>
2024-06-11 21:12:29 -0300

    Finish find the oldest exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    oldest = people.sort((a, b) => {
+        if(!a.yearOfDeath){
+          return (b.yearOfDeath - b.yearOfBirth) - (2024 - a.yearOfBirth)
 
+        } else if(!b.yearOfDeath){
+          return (2024 - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)
+
+        } else {
+          return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)
+        }
+    });
+    return oldest[0]
 };
 
 // Do not edit below this line
